e484f1d66c45e46d17271e2d0344d73c77f1c678
Foreverhighness <Foreverhighness@github.com>
2020-10-28 15:01:04 +0800

    :sparkles: using clang, make Propery.cpp complied.

--- Modern/0.Concepts/Properties.cpp
+++ Modern/0.Concepts/Properties.cpp
@@ -1,9 +1,26 @@
-// use clang.
+// use clang++ -fdeclspec -std=c++2a -Og -Wall Properties.cpp -o Properties
+#include <iostream>
+using namespace std;
 class Person {
     int age_;
 
    public:
-    int get_age() const { return age_; }
-    void set_age(int value) { age_ = value; }
-    // __declspec(property(get = get_age, put = set_age)) int age;
+    int get_age() const {
+        cout << "Get age " << age_ << endl;
+        return age_;
+    }
+    void set_age(int value) {
+        cout << "Set age to " << value << endl;
+        age_ = value;
+    }
+    __declspec(property(get = get_age, put = set_age)) int age;
 };
+void test() {
+    Person p;
+    p.age = 20;
+    cout << p.age << endl;
+}
+int main() {
+    test();
+    return 0;
+}
