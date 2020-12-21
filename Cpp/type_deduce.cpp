911f15331e2c57dbf2fe1c4e5b2868a863e6e0a3
Foreverhighness <Foreverhighness@github.com>
2020-12-21 10:14:10 +0800

    ✨ Display certain type.

---
+++ Cpp/type_deduce.cpp
@@ -0,0 +1,42 @@
+#include <boost/type_index.hpp>
+#include <iostream>
+#include <vector>
+#include <array>
+
+template <typename T>
+void show(const T& para) {
+  using boost::typeindex::type_id_with_cvr;
+  using std::cout;
+
+  // show T
+  cout << "T    = " << type_id_with_cvr<T>().pretty_name() << "\n";
+
+  // show para's type
+  cout << "para = " << type_id_with_cvr<decltype(para)>().pretty_name() << "\n";
+}
+
+class Widget {};
+
+std::vector<Widget> createVec() { return {{}, {}}; }
+
+int main() {
+  // const auto vec = createVec();
+  // if (!vec.empty()) {
+  //   show(&vec[0]);
+  //   show(vec[0]);
+  //   show(&vec);
+  //   show(vec);
+  // }
+  std::vector<bool> vb;
+  vb.emplace_back();
+  if (!vb.empty()) {
+    // show(&vb[0]);
+    show(vb[0]);
+    // show(&vb);
+    // show(vb);
+  }
+  std::array<bool, 1> ab;
+  show(ab[0]);
+
+  return 0;
+}
